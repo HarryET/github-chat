aef57851bcea24bcea9d47ef2eb92bfd8af787a0
Victor Peralta Reyes <peralta.r.victor@gmail.com>
2021-10-06 13:57:12 -0700

    delete unused file

--- service/serverSupabase.ts
+++
@@ -1,4 +0,0 @@
-import { createClient } from "@supabase/supabase-js";
-import { NEXT_PUBLIC_SUPABASE_URL, SUPABASE_SERVICE_KEY } from "env";
-
-export const serverSupabase = createClient(NEXT_PUBLIC_SUPABASE_URL, SUPABASE_SERVICE_KEY);
