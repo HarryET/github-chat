03b94c2ac2351ec9924dcf637ce6d3dfb308ddbf
hugo <hcgar00@gmail.com>
2021-10-10 01:45:59 +0300

    Revamp branding & tweak landing page

--- components/Discover.tsx
+++ components/Discover.tsx
@@ -1,8 +1,9 @@
-import { Box, ButtonPrimary, Heading, Text, TextInput } from "@primer/components";
+import { Box, ButtonPrimary, Heading, Link, Text, TextInput } from "@primer/components";
 import { useRouter } from "next/router";
 import React, { ChangeEvent, FormEvent, FormEventHandler, useState } from "react";
 import { useQuery } from "react-query";
 import { getActiveChats } from "service/supabase";
+import { buttonGradient } from "styles/styles";
 import { DiscoverMessage } from "./DiscoverMessage";
 
 export default function Discover() {
@@ -24,13 +25,11 @@ export default function Discover() {
     if (tryNowRepoPath?.includes("github.com/")) {
       const [afterDomain] = tryNowRepoPath?.toLowerCase().split("github.com/").slice(-1);
       const [owner, name] = afterDomain.split("/");
-      console.log(tryNowRepoPath, afterDomain, owner, name);
       if (owner && name) {
         router.push(`${owner}/${name}`);
       }
     } else {
       const [owner, name] = tryNowRepoPath?.split("/") || [];
-      console.log(tryNowRepoPath, owner, name);
       if (owner && name) {
         router.push(`${owner}/${name}`);
       }
@@ -46,17 +45,34 @@ export default function Discover() {
       alignItems="center"
       paddingX={[3, 4]}
       paddingY={4}
+
       // TODO Fix on mobile so scroll works
       // overflowY="scroll"
     >
-      <Box display="flex" flexDirection={["column", "column", "row"]} alignItems="center" maxWidth={1280} width="100%">
-        <Box display="flex" flexDirection="column" flex={1} mr={[0, 0, 6]}>
-          <Heading sx={{ fontSize: [5, 7], lineHeight: 1.25 }} textAlign={["center", "left"]}>
-            A chat room for every GitHub repository. <br />
-            Real-time.
-          </Heading>
-          <Box width={[1]} mt={4} display="flex" flexDirection="column">
-            <Text fontSize={3} fontWeight={300} color="fg.muted" textAlign={["center", "left"]}>
+      <Box
+        display="flex"
+        flexDirection={["column", "column", "row"]}
+        alignItems="center"
+        maxWidth={1280}
+        width="100%"
+        // border="1px dashed pink"
+      >
+        <Box
+          display="flex"
+          height="100%"
+          maxHeight={["none", "none", "75vh"]}
+          flexDirection="column"
+          justifyContent="space-between"
+          flex={1}
+          mr={[0, 0, 6]}
+        >
+          <Box />
+          <Box width={[1]} display="flex" flexDirection="column">
+            <Heading sx={{ fontSize: [5, 7], lineHeight: 1.25 }} textAlign={["center", "left"]}>
+              A chat room for every GitHub repository. <br />
+              Real-time.
+            </Heading>
+            <Text mt={4} fontSize={3} fontWeight={300} color="fg.muted" textAlign={["center", "left"]}>
               Paste a GitHub url or type a repository owner/name.
             </Text>
             <form onSubmit={handleFormSubmit}>
@@ -71,42 +87,79 @@ export default function Discover() {
                     sx={{ fontSize: 2, height: "48px", minWidth: ["250px"], flexGrow: 1 }}
                   />
                 </Box>
-                <ButtonPrimary height="48px" type="submit" variant="large" mt={[2, 2, 0]} ml={[0, 0, 2]}>
+                <ButtonPrimary
+                  height="48px"
+                  type="submit"
+                  variant="large"
+                  mt={[2, 2, 0]}
+                  ml={[0, 0, 2]}
+                  sx={{
+                    border: "none",
+                    background: buttonGradient.default,
+                    ":hover": {
+                      background: buttonGradient.hover,
+                    },
+                  }}
+                >
                   Chat now
                 </ButtonPrimary>
               </Box>
             </form>
           </Box>
+          <Text fontSize={1} color="fg.subtle">
+            A project by{" "}
+            <Link href="https://twitter.com/TheHarryET" target="_blank">
+              Harry
+            </Link>
+            ,{" "}
+            <Link href="https://twitter.com/PeraltaDev" target="_blank">
+              Victor
+            </Link>{" "}
+            &{" "}
+            <Link href="https://twitter.com/_hugocardenas" target="_blank">
+              Hugo
+            </Link>
+            .
+          </Text>
         </Box>
-        <Box flex={1} ml={[0, 0, 6]} mt={[6, 6, 0]} padding={[0, 0, 0]} width="100%">
-          <Box position="relative">
-            <Text display="inline-block" color="fg.muted" mb={3}>
-              Recent Messages
-            </Text>
-            <Box display="flex" flexDirection="column" maxHeight={["none", "none", "75vh"]} overflowY="auto" pb={8}>
-              {latestMessages &&
-                latestMessages.map((msg) => {
-                  return (
-                    <DiscoverMessage
-                      key={msg.id}
-                      avatar={msg.avatar_url}
-                      username={msg.username}
-                      content={msg.content}
-                      repoName={msg.repo_name}
-                      repoOwner={msg.repo_owner}
-                    />
-                  );
-                })}
-            </Box>
-            <Box
-              background="linear-gradient(rgba(0,0,0,0), rgba(13,17,23,0.7))"
-              display={["none", "none", "block"]}
-              position="absolute"
-              width="100%"
-              height="32px"
-              bottom={0}
-            />
+        <Box
+          display="flex"
+          flexDirection="column"
+          flex={1}
+          ml={[0, 0, 6]}
+          mt={[6, 6, 0]}
+          padding={[0, 0, 0]}
+          position="relative"
+          maxHeight={["none", "none", "75vh"]}
+          width="100%"
+          overflowY="hidden"
+        >
+          <Text display="inline-block" color="fg.muted" mb={3}>
+            Recent Messages
+          </Text>
+          <Box display="flex" flexDirection="column" overflowY="auto">
+            {latestMessages &&
+              latestMessages.map((msg) => {
+                return (
+                  <DiscoverMessage
+                    key={msg.id}
+                    avatar={msg.avatar_url}
+                    username={msg.username}
+                    content={msg.content}
+                    repoName={msg.repo_name}
+                    repoOwner={msg.repo_owner}
+                  />
+                );
+              })}
           </Box>
+          <Box
+            background="linear-gradient(rgba(0,0,0,0), rgba(13,17,23,0.7))"
+            display={["none", "none", "block"]}
+            position="absolute"
+            width="100%"
+            height="32px"
+            bottom={0}
+          />
         </Box>
       </Box>
     </Box>
