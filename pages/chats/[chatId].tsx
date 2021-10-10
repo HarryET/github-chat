2bdd0143646245773acc0cd36e5f47e2f65f0099
HarryET <29015545+HarryET@users.noreply.github.com>
2021-10-10 14:38:27 +0100

    Enable sidebar

--- pages/chats/[chatId].tsx
+++ pages/chats/[chatId].tsx
@@ -120,7 +120,7 @@ const ViewChat: NextPage = () => {
   return (
     <Root fixedScreenHeight={true}>
       <Box bg="canvas.default" display="flex" flexDirection="row" flexGrow={1} width="100%" maxWidth="100%">
-        {/* <SideMenu selectedChatId={chatId} /> */}
+        <SideMenu selectedChatId={chatId} />
         <Box display="flex" flexDirection="column" flexGrow={1}>
           {(isMessagesLoading || !!messagesError || (messages && messages.length === 0)) && (
             <Box
