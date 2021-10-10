9b746c6e3860d98502a323561ae6ca6224728717
HarryET <29015545+HarryET@users.noreply.github.com>
2021-10-10 16:51:06 +0100

    Change cursor

--- components/SocialIcons.tsx
+++ components/SocialIcons.tsx
@@ -7,17 +7,17 @@ import NextLink from "next/link";
 export const SocialIcons = (props: ComponentProps<typeof Box>) => (
     <Box display="flex" flexDirection="row" {...props}>
         <NextLink href={"https://github.com/HarryET/github-chat"}>
-            <Box>
+            <Box style={{cursor: "pointer"}}>
                 <FontAwesomeIcon icon={faGithub} size={"sm"} color={"white"} />
             </Box>
         </NextLink>
         <NextLink href={"https://twitter.com/GithubChat"}>
-            <Box ml={2}>
+            <Box ml={2} style={{cursor: "pointer"}}>
                 <FontAwesomeIcon icon={faTwitter} size={"sm"} color={"white"} />
             </Box>
         </NextLink>
         <NextLink href={"/discord"}>
-            <Box ml={2}>
+            <Box ml={2} style={{cursor: "pointer"}}>
                 <FontAwesomeIcon icon={faDiscord} size={"sm"} color={"white"} />
             </Box>
         </NextLink>
