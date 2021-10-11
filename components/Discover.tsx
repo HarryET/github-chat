0658bad626f1e296564af01745163f659fd88a28
hugo <hcgar00@gmail.com>
2021-10-11 06:06:00 +0300

    Disable icons causing flash of giant icon

--- components/Discover.tsx
+++ components/Discover.tsx
@@ -91,7 +91,7 @@ export const Discover = ({ repositories }: Props) => {
           </Box>
           <Box display="flex" flexDirection="column">
             <PersonalLinks sx={{ display: ["none", "none", "block"] }} />
-            <SocialIcons mt={2} sx={{ display: ["none", "none", "flex"] }} />
+            {/* <SocialIcons mt={2} sx={{ display: ["none", "none", "flex"] }} /> */}
           </Box>
         </Box>
         <Box
