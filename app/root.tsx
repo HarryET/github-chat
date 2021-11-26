cb22027be32e8a340b55571874845a55bf371b16
Harry Bairstow <h.bairstow22@gmail.com>
2021-11-26 18:53:25 +0000

    Move to remix
    * Move to new remix framework, this is because of the nested routing that it supports

---
+++ app/root.tsx
@@ -0,0 +1,114 @@
+import {
+  Link,
+  Links,
+  LiveReload,
+  Meta,
+  Outlet,
+  Scripts,
+  ScrollRestoration,
+  useCatch
+} from "remix";
+import type { MetaFunction } from "remix";
+
+export let meta: MetaFunction = () => {
+  return {
+    title: "GitHub Chat | A chat room for every GitHub repository",
+    description: "A chat room for every GitHub repository",
+    // Open graph & Facebook
+    "og:type": "website",
+    "og:url": "https://github-chat.com/",
+    "og:title": "GitHub Chat | A chat room for every GitHub repository",
+    "og:description": "A chat room for every GitHub repository",
+    "og:image": "https://github-chat.com/meta-image.png",
+    // Twitter
+    "twitter:card": "summary_large_image",
+    "twitter:url": "https://github-chat.com/",
+    "twitter:title": "GitHub Chat | A chat room for every GitHub repository",
+    "twitter:description": "A chat room for every GitHub repository",
+    "twitter:image": "https://github-chat.com/meta-image.png"
+  };
+};
+
+export default function App() {
+  return (
+    <Document>
+      <Outlet />
+    </Document>
+  );
+}
+
+export function ErrorBoundary({ error }: { error: Error }) {
+  console.error(error);
+  return (
+    <Document title="Error!">
+      <div>
+        <h1>There was an error</h1>
+        <p>{error.message}</p>
+        <hr />
+        <p>
+          Hey, developer, you should replace this with what you want your
+          users to see.
+        </p>
+      </div>
+    </Document>
+  );
+}
+
+export function CatchBoundary() {
+  let caught = useCatch();
+
+  let message;
+  switch (caught.status) {
+    case 401:
+      message = (
+        <p>
+          Oops! Looks like you tried to visit a page that you do not have access
+          to.
+        </p>
+      );
+      break;
+    case 404:
+      message = (
+        <p>Oops! Looks like you tried to visit a page that does not exist.</p>
+      );
+      break;
+
+    default:
+      throw new Error(caught.data || caught.statusText);
+  }
+
+  return (
+    <Document title={`${caught.status} ${caught.statusText}`}>
+      <h1>
+        {caught.status}: {caught.statusText}
+      </h1>
+      {message}
+    </Document>
+  );
+}
+
+function Document({
+  children,
+  title
+}: {
+  children: React.ReactNode;
+  title?: string;
+}) {
+  return (
+    <html lang="en">
+      <head>
+        <meta charSet="utf-8" />
+        <meta name="viewport" content="width=device-width,initial-scale=1" />
+        {title ? <title>{title}</title> : null}
+        <Meta />
+        <Links />
+      </head>
+      <body>
+        {children}
+        <ScrollRestoration />
+        <Scripts />
+        {process.env.NODE_ENV === "development" && <LiveReload />}
+      </body>
+    </html>
+  );
+}
---
+++ app/routes/index.tsx
@@ -0,0 +1,21 @@
+export default function Index() {
+  return (
+    <div>
+      <main>
+        <h2>Welcome to Remix!</h2>
+        <p>We're stoked that you're here. 🥳</p>
+        <p>
+          Feel free to take a look around the code to see how Remix does things,
+          it might be a bit different than what you’re used to. When you're
+          ready to dive deeper, we've got plenty of resources to get you
+          up-and-running quickly.
+        </p>
+        <p>
+          Check out all the demos in this starter, and then just delete the{" "}
+          <code>app/routes/demos</code> and <code>app/styles/demos</code>{" "}
+          folders when you're ready to turn this into your next project.
+        </p>
+      </main>
+    </div>
+  );
+}
