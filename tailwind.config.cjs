83bbe767364c6d9d8b7b33509741e7fb622ab09d
Harry Bairstow <me@harryet.xyz>
2022-12-10 15:45:19 +0000

    feat: styles

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -9,6 +9,13 @@ module.exports = {
         "sans": mona,
         "mona": mona,
         "hubot": `"Hubot Sans",${mona}`
+      },
+      colors: {
+        // 600
+        brand: { '50': '#FAF7F9', '100': '#F2EAEF', '200': '#E2D1DC', '300': '#D2B8C9', '400': '#C39FB6', '500': '#B386A3', '600': '#A36D90', '700': '#845373', '800': '#623E55', '900': '#402837' },
+        green: "#73A580",
+        amber: "#F4D35E",
+        red: "#CF4D6F",
       }
     },
   },
